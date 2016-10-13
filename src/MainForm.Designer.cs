20723caa54173897436042f81439458771ece39f
Douglas Zanco Franco <douglas@vizir.com.br>
2016-10-13 14:11:36 -0300

    change notfy icon

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -50,7 +50,7 @@
             this.notifyIcon1.BalloonTipText = "IdleSpy is working in background";
             this.notifyIcon1.BalloonTipTitle = "IdleSpy";
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Text = "IdleSpy";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
@@ -138,7 +138,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 258);
             this.Controls.Add(this.btnChart);
             this.Controls.Add(this.btnHistory);
             this.Controls.Add(this.panel1);
